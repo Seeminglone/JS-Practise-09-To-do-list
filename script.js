b0bbf7a3aaa62b20751b84392efca4c9ab3ead57
Seeminglone <yurakravtsov833@gmail.com>
2022-01-19 12:23:03 +0100

    SampleCode

--- script.js
+++ script.js
@@ -1,22 +1,77 @@
 const addBtn = document.querySelector('.add-btn');
-const deleteBtns = document.querySelector('.delete-btn');
-
+let deleteBtns;
+let deleteAllBtn;
+let editBtns;
+let editOverBtns;
 
 
 addBtn.addEventListener('click', function(){
-    const inp = document.getElementById('new-tasks').value;
-    const paragraph = document.querySelector('.paragraph');
-    const test = document.querySelector('#test');
-    test.innerHTML += `<div class="task">
-    <p class="paragraph">${inp}</p>
-    <button class="delete-btn">
-        <img src="icon.jpg" width="65" height="45">
-    </button>
-</div>>
-</button>`;
-    // paragraph.classList.toggle("open-to-do-task");
-    // deleteBtns.classList.toggle("open-delete-btn");
+ 
+    //Move frome input to paragraph    
+    let inp = document.getElementById('new-tasks');
+    let paragraph = document.querySelector('.paragraph');
+    if (inp.value) {
+            const test = document.querySelector('#test');
+            test.innerHTML += `<div class="task">
+            <b><p class="paragraph">${inp.value}</p></b>
+            <button class="edit-btn">
+            <i class="fas fa-edit"></i>
+            </button>
+            <button class="edit-over-btn">
+            <i class="fas fa-check"></i>
+            </button>
+            <button class="delete-btn">
+            <i class="fas fa-trash-alt"></i>
+            </button>
+                </div>`;
 
+            let arr = [];
+            let deleteArr = [];
+            arr.push(inp.value);
+            console.log(arr);
+                
+            
+
+        editBtns = document.querySelectorAll('.edit-btn').forEach(btn => {
+            btn.addEventListener('click', (event) => {
+                event = document.querySelector('#test').contentEditable = true;
+            })
+        });
+
+        editOverBtns = document.querySelectorAll('.edit-over-btn').forEach(btn => {
+            btn.addEventListener('click', (event) => {
+                event  = document.querySelector('#test').contentEditable = false;
+            })
+        });
+
+        
+
+        deleteBtns = document.querySelectorAll('.delete-btn').forEach(btn => {
+            btn.addEventListener('click', (event) => {
+            event.currentTarget.parentElement.remove();
+                if (!test.innerHTML){
+                    let closeAllBtn = document.querySelector('#close-all-button');
+                    closeAllBtn.innerHTML = '';
+                }
+            })
+        });
+
+        
+        deleteAllBtn = document.querySelector('#close-all-button');
+        deleteAllBtn.addEventListener('click', () => {
+            test.innerHTML = '';
+            deleteAllBtn.innerHTML = '';
+             
+        })
+        
+        let closeAllBtn = document.querySelector('#close-all-button');
+            closeAllBtn.innerHTML = `<div class ="close-all-btn-container">
+            <button class="delete-all-btn"><i class="fas fa-times"></i></button>
+            </div>`;
+            
+            inp.value = "";       
+    } else {
+        return;
+    }
 });
-
 
